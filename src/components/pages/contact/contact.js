26e3a39f68168983303b6ce4617cbee825997b2c
Lee Charlton-Hassall <leehassall@hotmail.co.uk>
2022-02-07 17:20:24 +0000

    Added contact form

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -4,7 +4,39 @@ import ContentSection from "../../common/contentSection/contentSection";
 function Contact() {
   return (
     <ContentSection id="contact" h2="Contact">
-
+      <div className={styles.formContainer}>
+        <form
+          action="https://formspree.io/f/mvodevrl"
+          method="post"
+          enctype="multipart/form-data"
+        >
+          <div className={styles.formField}>
+            <label htmlFor="fname">First name *</label>
+            <input type="text" id="fname" minLength="2" required />
+          </div>
+          <div className={styles.formField}>
+            <label htmlFor="lname">Last name *</label>
+            <input type="text" id="lname" required />
+          </div>
+          <div className={styles.formField}>
+            <label htmlFor="email">Email *</label>
+            <input type="email" id="email" required />
+          </div>
+          <div className={styles.formField}>
+            <label htmlFor="message">Message</label>
+            <textarea
+              rows='5'
+              id="message"
+            ></textarea>
+          </div>
+          <div className={styles.formField}>
+            <button>Submit</button>
+          </div>
+        </form>
+        <div className={styles.contactFormImage}>
+          <img src={require("../../../assets/images/Edd.JPG")}alt="Picture of Edd" />
+        </div>
+      </div>
     </ContentSection>
   );
 }
